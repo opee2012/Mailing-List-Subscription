a7a2839832ae64a03a2629ec6b764a5c2c19188b
Jacob Borth <borthj2@winthrop.edu>
2022-11-27 18:20:15 -0500

    About half way done. Sends emails with link.

--- confirm.php
+++ confirm.php
@@ -1,1 +1,34 @@
 <?php
+    $confirm = $_GET["confirmation"];
+
+    function csvToArray(): array {
+        $lines[] = null;
+        if (file_exists("subscriptions.csv")) {
+            $file = fopen("subscriptions.csv", 'r');
+            while (!feof($file)) {
+                $lines[] = fgetcsv($file, 1000);
+            }
+            fclose($file);
+        }
+        return $lines;
+    }
+
+
+    $csv = csvToArray();
+
+    foreach ($csv as $line){
+        if ($line[1] == $confirm){
+            echo $confirm;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<HTML lang="en">
+<HEAD>
+    <TITLE>Confirmation</TITLE>
+</HEAD>
+<BODY>
+<H3>Thank you for subscribing!</H3>
+</BODY>
+</HTML>
--- subscribe.php
+++ subscribe.php
@@ -23,7 +23,10 @@
             $email_Err = urlencode($emailErr);
             header("Location: subscribe.php?emailErr=$email_Err");
         } else {
-            $random = bin2hex(random_bytes(10));
+            try {
+                $random = bin2hex(random_bytes(10));
+            } catch (Exception $e) {
+            }
 
             $openFile = fopen('subscriptions.csv', 'a');
             fwrite($openFile, $email . "," . $random . ",false\n");
@@ -31,13 +34,21 @@
             fclose($openFile);
 
             $confirmation = ("Confirmation email sent to " . urlencode($email));
-            header("Location: subscribe.php?confirmation=$confirmation");
+            header("Location: subscribe.php?confirm=$confirmation");
+
+            $msg = "Confirm your Mailing List subscription by clicking the link below.
+                    \nhttp://localhost:8081/confirm.php?confirmation=$random
+                    \nThank you,\nMailing List Company";
+
+            $msg = wordwrap($msg, 70);
+
+            mail($email, "Confirm Subscription to Mailing List", $msg);
         }
     } else {
 ?>
 
 <!DOCTYPE html>
-<HTML>
+<HTML lang="en">
 <HEAD>
     <TITLE>Subscribe</TITLE>
     <STYLE>.error {color: red; font-style: italic}</STYLE>
@@ -55,7 +66,7 @@
     </TABLE>
     <INPUT type="submit" value="Subscribe">
     <INPUT type="reset">
-    <SPAN class="confirm"><?php echo $_GET['confirmation'];?></SPAN>
+    <SPAN class="confirm"><?php echo $_GET['confirm'];?></SPAN>
 </FORM>
 </BODY>
 </HTML>
